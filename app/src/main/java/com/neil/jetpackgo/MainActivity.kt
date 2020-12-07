589221b0a2bde5fd20771f9fb98ea0d5fd1eeee3
neil.ling <neil.ling@aylaasia.com>
2020-12-07 16:26:56 +0800

    set contents with bound views

--- app/src/main/java/com/neil/jetpackgo/MainActivity.kt
+++ app/src/main/java/com/neil/jetpackgo/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.neil.jetpackgo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.neil.jetpackgo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.welcome.text = "Hello View Binding"
+
+        setContentView(binding.root)
     }
 }
