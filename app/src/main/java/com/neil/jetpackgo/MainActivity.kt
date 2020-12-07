3affbe413f040629b761a5e58d5db376942368f2
neil.ling <neil.ling@aylaasia.com>
2020-12-07 17:33:25 +0800

    make binding view available

--- app/src/main/java/com/neil/jetpackgo/MainActivity.kt
+++ app/src/main/java/com/neil/jetpackgo/MainActivity.kt
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import com.neil.jetpackgo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.greeting.text = "Hello View Binding"
-
         setContentView(binding.root)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        binding.greeting.text = "Hello View Binding"
     }
 }
