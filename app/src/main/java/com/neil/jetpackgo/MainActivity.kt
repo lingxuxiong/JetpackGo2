a17458d6a1fd42a2b3580218bf5109e09ec68e6d
neil.ling <neil.ling@aylaasia.com>
2020-12-08 17:22:18 +0800

    inflates root view in a data binding way and change value of the bound variable

--- app/src/main/java/com/neil/jetpackgo/MainActivity.kt
+++ app/src/main/java/com/neil/jetpackgo/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.neil.jetpackgo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.neil.jetpackgo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+        binding.greeting = "Hello DataBinding"
     }
 }
